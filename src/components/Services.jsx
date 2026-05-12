68536bbc1eef469a678f25245b6f0f7744b46a09
FarkasGyovaiMonika <gyovaim28@gmail.com>
2026-05-12 22:00:09 +0200

    szolgáltatások módosítás

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -44,22 +44,12 @@ function Services() {
       </div>
     </section>
   );
-  function ServiceCard({ title, description, items, buttonText }) {
+  function ServiceCard({ icon, title, description, items, buttonText }) {
     return (
       <div className="services-card">
         <div className="services-svg">
           {/* ide jön majd az SVG */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-          >
-            <path d="M9 5h-2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-12a2 2 0 0 0-2-2h-2" />
-          </svg>
+          {icon}
         </div>
 
         <h3>{title}</h3>
