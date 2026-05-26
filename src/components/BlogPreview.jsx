e2b3a931c3cd40ee535b38e1f7088613973107df
FarkasGyovaiMonika <gyovaim28@gmail.com>
2026-05-26 18:35:35 +0200

    blogPreview kiegészÍtés img-vel

--- src/components/BlogPreview.jsx
+++ src/components/BlogPreview.jsx
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 function BlogPreview({ link, image, cim, excerpt }) {
   return (
     <div id="services" className="content-box">
-      <div className="services-card">
+      <div className="blog-preview services-card">
+        <img src={image} />
         <h2>{cim}</h2>
         <div>
           <p>{excerpt}</p>
