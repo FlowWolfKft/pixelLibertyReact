71abd0d70f6ed986c3ac4098133727026f3d57a1
FarkasGyovaiMonika <gyovaim28@gmail.com>
2026-05-28 09:48:50 +0200

    Scrolltop beállítás

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import ScrollToTop from "./components/ScrollToTop";
 import { BrowserRouter } from "react-router-dom";
 
 import "./menu.css";
@@ -11,6 +12,7 @@ import "./blog.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>,
 );
